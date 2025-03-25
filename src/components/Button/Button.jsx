068784d9178da8d72631cca92a98963d72d7975b
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-25 17:53:08 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,11 +5,15 @@ import { buttonClass, buttonConfig } from "./Button.config";
 export const Button = ({
   variant = "primary",
   size = "md",
+  className = "",
   children,
   ...rest
 }) => {
   return (
-    <button className={buttonClass({ variant, size })} {...rest}>
+    <button
+      className={`${buttonClass({ variant, size })} ${className}`}
+      {...rest}
+    >
       {children}
     </button>
   );
@@ -19,5 +23,6 @@ Button.propTypes = {
   variant: PropTypes.oneOf(Object.keys(buttonConfig.variants.variant))
     .isRequired,
   size: PropTypes.oneOf(Object.keys(buttonConfig.variants.size)).isRequired,
+  className: PropTypes.string,
   children: PropTypes.node,
 };
