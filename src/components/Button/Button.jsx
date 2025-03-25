4f668516955471feffcfad101e46f2e85ece09c1
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-03-25 22:47:53 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,26 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import { buttonClass, buttonConfig } from "./Button.config";
 
 export const Button = ({
   variant = "primary",
   size = "md",
+  to = "",
   className = "",
   children,
   ...rest
 }) => {
+  const ElementType = to === "" ? "button" : Link;
+
   return (
-    <button
+    <ElementType
+      to={to === "" ? null : to}
       className={`${buttonClass({ variant, size })} ${className}`}
       {...rest}
     >
       {children}
-    </button>
+    </ElementType>
   );
 };
 
@@ -23,6 +28,7 @@ Button.propTypes = {
   variant: PropTypes.oneOf(Object.keys(buttonConfig.variants.variant))
     .isRequired,
   size: PropTypes.oneOf(Object.keys(buttonConfig.variants.size)).isRequired,
+  to: PropTypes.string,
   className: PropTypes.string,
   children: PropTypes.node,
 };
