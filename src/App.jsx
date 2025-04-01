19d283d2ebba63a40c5a88eee0309db7c3c79117
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-01 04:22:17 +0300

    Move providers from App to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/routes";
-import { MediaQueriesProvider } from "@/context/mediaQueriesContext";
 
-function App() {
-  return (
-    <MediaQueriesProvider>
-      <RouterProvider router={router} />
-    </MediaQueriesProvider>
-  );
+export function App() {
+  return <RouterProvider router={router} />;
 }
-export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { MediaQueriesProvider } from "@/context/mediaQueriesContext";
+
+import { App } from "./App";
 import "./index.scss";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <MediaQueriesProvider>
+      <App />
+    </MediaQueriesProvider>
   </StrictMode>
 );
