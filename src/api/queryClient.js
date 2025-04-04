d5e42a80e6ba276e7fd7362b74dbfc5bee7754c7
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-05 01:56:58 +0300

    Create queryClient.js

---
+++ src/api/queryClient.js
@@ -0,0 +1,13 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2,
+      retry: 1,
+      refetchOnWindowFocus: false,
+      // gcTime: 1000 * 60 * 5,
+      // refetchOnReconnect: true,
+    },
+  },
+});
