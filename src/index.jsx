d8522613a46548db5a2516042e22e0d0c6a8aefe
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-05 01:57:07 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 import { MediaQueriesProvider } from "@/context/mediaQueriesContext";
+import { queryClient } from "@/api/queryClient";
 
 import { App } from "./App";
 import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MediaQueriesProvider>
-      <App />
-    </MediaQueriesProvider>
+    <QueryClientProvider client={queryClient}>
+      <MediaQueriesProvider>
+        <App />
+      </MediaQueriesProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
