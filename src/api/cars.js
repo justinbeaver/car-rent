558082c3c7fa6799402dab9921095c3aa39f4af4
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-05 17:16:20 +0300

    Update cars.js

--- src/api/cars.js
+++ src/api/cars.js
@@ -4,5 +4,8 @@ export const carsApi = {
   getAll: ({ limit = 10 } = {}) =>
     api.get("/cars", { params: { _limit: limit } }),
 
-  getById: (id) => api.get(`/cars/${id}`, { params: { _embed: "carType" } }),
+  getById: (id) =>
+    api.get(`/cars/${id}`, {
+      params: { _embed: ["carType", "carDescription"] },
+    }),
 };
