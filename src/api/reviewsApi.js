9198995c6ec6f4e747d295f612c3cfa61bd6acfa
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-06 21:48:59 +0300

    Update reviewsApi.js

--- src/api/reviewsApi.js
+++ src/api/reviewsApi.js
@@ -1,6 +1,8 @@
 import { api } from "./apiClient";
 
 export const reviewsApi = {
-  getByCarId: (id) =>
-    api.get("/reviews", { params: { carId: id, _embed: "user" } }),
+  getByCarId: (id, { page = 1, perPage = 10 } = {}) =>
+    api.get("/reviews", {
+      params: { carId: id, _embed: "user", _page: page, _per_page: perPage },
+    }),
 };
