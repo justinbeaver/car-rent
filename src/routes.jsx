f23f795bbe24a0acfb6e877ea57d6f5edfa6f91c
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-06 23:21:31 +0300

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { Layout } from "@/pages/Layout/Layout";
 import { Home } from "@/pages/Home/Home";
+import { Cars } from "@/pages/Cars/Cars";
+import { Car } from "@/pages/Car/Car";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "cars",
+        element: <Cars />,
+      },
+      {
+        path: "cars/:carId",
+        element: <Car />,
+      },
+    ],
   },
 ]);
