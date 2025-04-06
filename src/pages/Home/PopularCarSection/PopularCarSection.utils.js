59cf54e120e31d1f8e53115a0e6b0ae90ce6cbee
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-07 02:11:10 +0300

    Delete PopularCarSection.utils.js

--- src/pages/Home/PopularCarSection/PopularCarSection.utils.js
+++
@@ -1,8 +0,0 @@
-import { allCars } from "@/data/allCars";
-import { popularCarIds } from "@/data/popularCars";
-
-export const getPopularCars = () => {
-  const popularCars = allCars.filter((car) => popularCarIds.includes(car.id));
-
-  return popularCars;
-};
