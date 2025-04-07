7104d0d9497d57ff5699da3654c30129940d034e
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-07 03:17:02 +0300

    Update RecommendationCarSection.jsx

--- src/pages/Home/RecommendationCarSection/RecommendationCarSection.jsx
+++ src/pages/Home/RecommendationCarSection/RecommendationCarSection.jsx
@@ -1,32 +1,24 @@
 import { CarsGrid } from "@/components/CarsGrid/CarsGrid";
-import { CarCard } from "@/components/CarCard/CarCard";
-import { useMediaQueriesContext } from "@/context/mediaQueriesContext";
 
-import { getRecommendedCars } from "./RecommendationCarSection.utils";
+import { useCarCardsList } from "./RecommendationCarSection.hooks";
 import s from "./RecommendationCarSection.module.scss";
 
 export const RecommendationCarSection = () => {
-  const { isSm } = useMediaQueriesContext();
-  const recommendedCars = getRecommendedCars();
-
-  const cardsList = recommendedCars
-    .slice(0, 8)
-    .map(({ id, images, ...rest }) => (
-      <CarCard
-        key={id}
-        variant={isSm ? "vertical" : "horizontal"}
-        carId={id}
-        imgUrl={images[0]}
-        {...rest}
-      />
-    ));
+  const { cardsList, isLoading, isError, error } = useCarCardsList({
+    limit: 8,
+  });
 
   return (
     <section>
       <header className={s["section__header"]}>
         <h3 className={s["section__heading"]}>Recommendation Car</h3>
       </header>
-      <CarsGrid size="sm" cardsList={cardsList} />
+
+      {isLoading && <p>Loading...</p>}
+
+      {isError && <p>{error.message}</p>}
+
+      {!isLoading && !isError && <CarsGrid size="sm" cardsList={cardsList} />}
     </section>
   );
 };
