9b8a7164517f13a41c26e92d3f47af16024d1df1
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-07 04:13:28 +0300

    Update ShowMoreCarsSection.jsx

--- src/pages/Home/ShowMoreCarsSection/ShowMoreCarsSection.jsx
+++ src/pages/Home/ShowMoreCarsSection/ShowMoreCarsSection.jsx
@@ -1,20 +1,22 @@
 import { Button } from "@/components/Button/Button";
 
-import { getTotalCarsCount } from "./ShowMoreCarsSection.utils";
+import { useTotalCarsCount } from "./ShowMoreCarsSection.hooks";
 import s from "./ShowMoreCarsSection.module.scss";
 
 export const ShowMoreCarsSection = () => {
-  const totalCarsCount = getTotalCarsCount();
+  const { totalCarsCount } = useTotalCarsCount();
 
   return (
     <div className={s["show-more"]}>
       <Button to="/cars" className={s["show-more__button"]}>
         Show more cars
       </Button>
-      <p className={s["show-more__count"]}>
-        <span className="visually-hidden">Total cars: </span>
-        <span>{`${totalCarsCount} Car`}</span>
-      </p>
+      {totalCarsCount && (
+        <p className={s["show-more__count"]}>
+          <span className="visually-hidden">Total cars: </span>
+          <span>{`${totalCarsCount} Car`}</span>
+        </p>
+      )}
     </div>
   );
 };
