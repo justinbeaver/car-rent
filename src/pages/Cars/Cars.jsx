2095a640be32f0c1fbd67808b3f0204b4616ee66
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-09 16:27:20 +0300

    Update Cars page

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,33 +1,68 @@
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 import { Wrapper } from "@/components/Wrapper/Wrapper";
+import { Spacer } from "@/components/Spacer/Spacer";
 import { CarsGrid } from "@/components/CarsGrid/CarsGrid";
 import { CarCard } from "@/components/CarCard/CarCard";
+import { Pagination } from "@/components/Pagination/Pagination";
 import { useAllCarsQuery } from "@/hooks/useAllCarsQuery";
 
+import s from "./Cars.module.scss";
+
 export const Cars = () => {
-  const [page] = useState(1);
+  const [searchParams] = useSearchParams();
+  const page = parseInt(searchParams.get("page")) || 1;
 
   const { data, isLoading, isError, error } = useAllCarsQuery({
     page,
     perPage: 3,
   });
 
-  const cardsList = data?.data.map(({ id, images, ...rest }) => (
-    <CarCard key={id} carId={id} imgUrl={images[0].url} {...rest} />
-  ));
+  const { last, items: totalCarsCount, data: cars } = data || {};
+
+  const cardsList =
+    cars?.map(({ id, images, ...rest }) => (
+      <CarCard key={id} carId={id} imgUrl={images[0].url} {...rest} />
+    )) || [];
+
+  const generateLinkForPage = (page) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("page", page);
+
+    return `?${newSearchParams.toString()}`;
+  };
 
   return (
     <>
       <aside></aside>
       <div>
         <Wrapper>
+          {/* TODO: should be a separate section component */}
           <section>
             {isLoading && <p>Loading...</p>}
 
             {isError && <p>{error.message}</p>}
 
-            {!isLoading && !isError && <CarsGrid cardsList={cardsList} />}
+            {!isLoading && !isError && (
+              <>
+                <CarsGrid cardsList={cardsList} />
+                <Spacer size="950" />
+                <div className={s.footer}>
+                  <Pagination
+                    className={s.pagination}
+                    current={page}
+                    last={last}
+                    generateLinkForPage={generateLinkForPage}
+                  />
+                  <p className={s["total-cars-count"]}>
+                    {/* TODO: make a separate total cars component with api call */}
+                    <span className="visually-hidden">Total cars: </span>
+                    <span>{`${totalCarsCount} Car`}</span>
+                  </p>
+                </div>
+                <Spacer size="950" />
+              </>
+            )}
           </section>
         </Wrapper>
       </div>
