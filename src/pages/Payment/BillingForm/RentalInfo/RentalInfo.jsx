366b08fcda59bf11696a2133796b70787d3a9736
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-13 21:13:40 +0300

    Update RentalInfo.jsx

--- src/pages/Payment/BillingForm/RentalInfo/RentalInfo.jsx
+++ src/pages/Payment/BillingForm/RentalInfo/RentalInfo.jsx
@@ -1,6 +1,11 @@
+import { Spacer } from "@/components/Spacer/Spacer";
+import { useMediaQueriesContext } from "@/context/mediaQueriesContext";
+
 import s from "./RentalInfo.module.scss";
 
 export const RentalInfo = () => {
+  const { isSm } = useMediaQueriesContext();
+
   return (
     <section className={s.section}>
       <header className={s.header}>
@@ -10,45 +15,100 @@ export const RentalInfo = () => {
         </div>
         <p className={s["step-status"]}>Step 2 of 4</p>
       </header>
-      <h3 className={s["subcategory-heading"]}>Pick-Up</h3>
-      <ul className={s.list}>
-        <li className={s.list__item}>
-          <label className={s.label} htmlFor="pick-up-location">
-            Locations
-          </label>
-          <select id="pick-up-location" className={s["select-input"]} required>
-            <option value="" selected disabled>
-              Select your city
-            </option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-          </select>
-        </li>
-        <li className={s.list__item}>
-          <label className={s.label} htmlFor="pick-up-time">
-            Time
-          </label>
-          <select id="pick-up-time" className={s["select-input"]} required>
-            <option value="" selected disabled>
-              Select your time
-            </option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-          </select>
-        </li>
-        <li className={s.list__item}>
-          <label className={s.label} htmlFor="pick-up-date">
-            Date
-          </label>
-          <select id="pick-up-date" className={s["select-input"]} required>
-            <option value="" selected disabled>
-              Select your date
-            </option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-          </select>
-        </li>
-      </ul>
+
+      <fieldset className={s.fieldset}>
+        <legend className={s.legend}>Pick-Up</legend>
+        <ul className={s.list}>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-location">
+              Locations
+            </label>
+            <select
+              id="pick-up-location"
+              className={s["select-input"]}
+              required
+            >
+              <option value="" selected disabled>
+                Select your city
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-time">
+              Time
+            </label>
+            <select id="pick-up-time" className={s["select-input"]} required>
+              <option value="" selected disabled>
+                Select your time
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-date">
+              Date
+            </label>
+            <select id="pick-up-date" className={s["select-input"]} required>
+              <option value="" selected disabled>
+                Select your date
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+        </ul>
+      </fieldset>
+
+      <Spacer size={isSm ? "600" : "500"} />
+
+      <fieldset className={s.fieldset}>
+        <legend className={s.legend}>Drop-Off</legend>
+        <ul className={s.list}>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-location">
+              Locations
+            </label>
+            <select
+              id="pick-up-location"
+              className={s["select-input"]}
+              required
+            >
+              <option value="" selected disabled>
+                Select your city
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-time">
+              Time
+            </label>
+            <select id="pick-up-time" className={s["select-input"]} required>
+              <option value="" selected disabled>
+                Select your time
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+          <li className={s.list__item}>
+            <label className={s.label} htmlFor="pick-up-date">
+              Date
+            </label>
+            <select id="pick-up-date" className={s["select-input"]} required>
+              <option value="" selected disabled>
+                Select your date
+              </option>
+              <option value="1">Option 1</option>
+              <option value="2">Option 2</option>
+            </select>
+          </li>
+        </ul>
+      </fieldset>
     </section>
   );
 };
