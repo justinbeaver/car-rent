b468f979c929ec310c28b690a249f4b7c250cff3
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-13 17:10:46 +0300

    Create Payment.jsx

---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,13 @@
+import { Wrapper } from "@/components/Wrapper/Wrapper";
+
+import { BillingForm } from "./BillingForm/BillingForm";
+
+export const Payment = () => {
+  return (
+    <>
+      <Wrapper>
+        <BillingForm />
+      </Wrapper>
+    </>
+  );
+};
