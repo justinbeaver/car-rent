158ba74ecc7b8fd100d8c0720c41ed09ca79e913
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-13 22:54:26 +0300

    Update PaymentMethod.jsx

--- src/pages/Payment/BillingForm/PaymentMethod/PaymentMethod.jsx
+++ src/pages/Payment/BillingForm/PaymentMethod/PaymentMethod.jsx
@@ -1,3 +1,7 @@
+import VisaLogo from "@/assets/logos/visa.svg?react";
+import PayPalLogo from "@/assets/logos/paypal.svg?react";
+import BitcoinLogo from "@/assets/logos/bitcoin.svg?react";
+
 import s from "./PaymentMethod.module.scss";
 
 export const PaymentMethod = () => {
@@ -10,6 +14,45 @@ export const PaymentMethod = () => {
         </div>
         <p className={s["step-status"]}>Step 3 of 4</p>
       </header>
+
+      <ul className={s.list}>
+        <li className={s["list__item"]}>
+          <label className={s.label} htmlFor="payment-credit-card">
+            <input
+              id="payment-credit-card"
+              type="radio"
+              name="payment"
+              value="credit-card"
+            />
+            <span>Credit Card</span>
+            <VisaLogo className={s["label-logo"]} aria-hidden="true" />
+          </label>
+        </li>
+        <li className={s["list__item"]}>
+          <label className={s.label} htmlFor="payment-paypal">
+            <input
+              id="payment-paypal"
+              type="radio"
+              name="payment"
+              value="paypal"
+            />
+            <span>PayPal</span>
+            <PayPalLogo className={s["label-logo"]} aria-hidden="true" />
+          </label>
+        </li>
+        <li className={s["list__item"]}>
+          <label className={s.label} htmlFor="payment-bitcoin">
+            <input
+              id="payment-bitcoin"
+              type="radio"
+              name="payment"
+              value="bitcoin"
+            />
+            <span>Bitcoin</span>
+            <BitcoinLogo className={s["label-logo"]} aria-hidden="true" />
+          </label>
+        </li>
+      </ul>
     </section>
   );
 };
