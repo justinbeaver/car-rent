09ba5cdd01b79799a5693d47b11ed62de9949bfe
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-14 14:15:20 +0300

    Update BillingForm.jsx

--- src/pages/Payment/BillingForm/BillingForm.jsx
+++ src/pages/Payment/BillingForm/BillingForm.jsx
@@ -2,10 +2,11 @@ import { BillingInfo } from "./BillingInfo/BillingInfo";
 import { RentalInfo } from "./RentalInfo/RentalInfo";
 import { PaymentMethod } from "./PaymentMethod/PaymentMethod";
 import { Confirmation } from "./Confirmation/Confirmation";
+import s from "./BillingForm.module.scss";
 
 export const BillingForm = () => {
   return (
-    <form>
+    <form className={s.form}>
       <BillingInfo />
       <RentalInfo />
       <PaymentMethod />
