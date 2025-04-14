b3937d2c2f61ad2cf43b9872ebb6776b1ea32994
justinbeaver <13700367+justinbeaver@users.noreply.github.com>
2025-04-14 14:28:28 +0300

    Update Payment.jsx

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,15 +1,20 @@
 import { Wrapper } from "@/components/Wrapper/Wrapper";
 import { Spacer } from "@/components/Spacer/Spacer";
 
+import { RentalSummary } from "./RentalSummary/RentalSummary";
 import { BillingForm } from "./BillingForm/BillingForm";
+import s from "./Payment.module.scss";
 
 export const Payment = () => {
   return (
     <>
       <Wrapper>
-        <Spacer size="500" />
-        <BillingForm />
-        <Spacer size="500" />
+        <Spacer size="600" />
+        <div className={s.content}>
+          <RentalSummary />
+          <BillingForm />
+        </div>
+        <Spacer size="600" />
       </Wrapper>
     </>
   );
